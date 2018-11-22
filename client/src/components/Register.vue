56bace0c48ebdb985232058880727e0b6b666ec1
JackFisherDev <yevgenrybalchenko@gmail.com>
2018-11-22 15:50:14 +0400

    login form layout

---
+++ client/src/components/Register.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-form ref="form">
+          <v-text-field
+            v-model="email"
+            label="E-mail"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            label="Password"
+            required
+          ></v-text-field>
+          <v-btn
+            @click="register"
+          >
+            sign in
+          </v-btn>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
