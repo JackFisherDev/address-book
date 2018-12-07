c773dd7851a105508b9a1451ac176f5af665938e
JackFisherDev <yevgenrybalchenko@gmail.com>
2018-12-07 15:32:13 +0400

    validation of data on contact page

--- client/src/components/Contact.vue
+++ client/src/components/Contact.vue
@@ -43,91 +43,100 @@
               </v-list>
             </v-menu>
           </v-card-title>
-          <v-layout
-            row
-            wrap
-            stretch
-            class="mt-3"
+          <v-form
+            ref="contactForm"
+            v-model="contactForm"
+            lazy-validation
           >
-            <v-flex
-              xs12
-              sm4
-              text-xs-center
-              class="pl-4 pr-4"
+            <v-layout
+              row
+              wrap
+              stretch
+              class="mt-3"
             >
-              <v-avatar
-                :size="120"
-                color="grey lighten-4"
+              <v-flex
+                xs12
+                sm4
+                text-xs-center
+                class="pl-4 pr-4"
               >
-                <img
-                  :src="contact.avatar"
-                  :alt="contact.name"
+                <v-avatar
+                  :size="120"
+                  color="grey lighten-4"
                 >
-              </v-avatar>
-            </v-flex>
-            <v-flex
-              xs12
-              sm8
-              class="pl-4 pr-4"
-              align-content-center
-            >
-              <v-text-field
-                  v-model="contact.name"
-                  :readonly="!enabledEditMode"
-                  prepend-icon="person"
-                  :append-icon="enabledEditMode ? 'edit' : ''"
-                  label="Contact name"
-                ></v-text-field>
-              <v-chip
-                small
-                color="success"
-                text-color="white"
-              >{{ contact.group }}</v-chip>
-            </v-flex>
-          </v-layout>
-          <v-layout
-            row
-            class="mt-3"
-          >
-            <v-flex
-              xs12
-              class="pl-4 pr-4"
-            >
-              <h6 class="title mt-2 mb-3">Contact info</h6>
-              <div>
+                  <img
+                    :src="contact.avatar"
+                    :alt="contact.name"
+                  >
+                </v-avatar>
+              </v-flex>
+              <v-flex
+                xs12
+                sm8
+                class="pl-4 pr-4"
+                align-content-center
+              >
                 <v-text-field
-                  v-model="contact.email"
-                  :readonly="!enabledEditMode"
-                  prepend-icon="email"
-                  :append-icon="enabledEditMode ? 'edit' : ''"
-                  label="Email"
-                ></v-text-field>
-              </div>
-              <div>
-                <v-text-field
-                  v-model="contact.phoneNumber"
-                  :readonly="!enabledEditMode"
-                  prepend-icon="phone"
-                  :append-icon="enabledEditMode ? 'edit' : ''"
-                  label="Phone Number"
-                ></v-text-field>
-              </div>
-              <div v-if="enabledEditMode">
-                <v-btn
-                  flat
-                  @click="enabledEditMode = false"
-                >
-                  Cancel
-                </v-btn>
-                <v-btn
-                  class="info"
-                  @click="updateContact(contact)"
-                >
-                  Save
-                </v-btn>
-              </div>
-            </v-flex>
-          </v-layout>
+                    v-model="contact.name"
+                    :rules="nameRules"
+                    :readonly="!enabledEditMode"
+                    prepend-icon="person"
+                    :append-icon="enabledEditMode ? 'edit' : ''"
+                    label="Contact name"
+                  ></v-text-field>
+                <v-chip
+                  small
+                  color="success"
+                  text-color="white"
+                >{{ contact.group }}</v-chip>
+              </v-flex>
+            </v-layout>
+            <v-layout
+              row
+              class="mt-3"
+            >
+              <v-flex
+                xs12
+                class="pl-4 pr-4"
+              >
+                <h6 class="title mt-2 mb-3">Contact info</h6>
+                <div>
+                  <v-text-field
+                    v-model="contact.email"
+                    :rules="emailRules"
+                    :readonly="!enabledEditMode"
+                    prepend-icon="email"
+                    :append-icon="enabledEditMode ? 'edit' : ''"
+                    label="Email"
+                  ></v-text-field>
+                </div>
+                <div>
+                  <v-text-field
+                    v-model="contact.phoneNumber"
+                    :rules="phoneNumberRules"
+                    :readonly="!enabledEditMode"
+                    prepend-icon="phone"
+                    :append-icon="enabledEditMode ? 'edit' : ''"
+                    label="Phone Number"
+                  ></v-text-field>
+                </div>
+                <div v-if="enabledEditMode">
+                  <v-btn
+                    flat
+                    @click="enabledEditMode = false"
+                  >
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                    class="info"
+                    @click="updateContact(contact)"
+                  >
+                    Save
+                  </v-btn>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-form>
         </v-card>
       </v-flex>
     </v-layout>
@@ -141,7 +150,19 @@ export default {
   data () {
     return {
       contact: {},
-      enabledEditMode: false
+      contactForm: true,
+      enabledEditMode: false,
+      // Form validation
+      nameRules: [
+        name => !!name || 'Please, enter contact name.'
+      ],
+      emailRules: [
+        v => !!v || 'Please, enter E-mail address.',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid.'
+      ],
+      phoneNumberRules: [
+        pN => !!pN || 'Please, enter phone number.'
+      ]
     }
   },
 
@@ -153,9 +174,16 @@ export default {
 
   methods: {
     async updateContact (contact) {
-      await ContactsService.updateContact(contact)
+      if (!this.$refs.contactForm.validate()) return
 
-      this.setContact(contact.id)
+      try {
+        await ContactsService.updateContact(contact)
+
+        this.setContact(contact.id)
+        this.enabledEditMode = false
+      } catch (err) {
+        console.log(err)
+      }
     },
 
     async setContact (contactId) {
